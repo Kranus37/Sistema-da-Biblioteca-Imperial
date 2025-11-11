95a5ca8a983bb3ae2024dd44a32560c0f1365f2b
Kranus37 <samuel.resende6637@gmail.com>
2025-11-11 19:35:08 -0300

    Update CORRIGIR_ESTADO_CONSERVACAO.sql

--- database/CORRIGIR_ESTADO_CONSERVACAO.sql
+++ database/CORRIGIR_ESTADO_CONSERVACAO.sql
@@ -1,22 +1,11 @@
 USE biblioteca_imperial;
 
--- Desabilitar safe mode temporariamente
 SET SQL_SAFE_UPDATES = 0;
 
--- Corrigir valores
 UPDATE exemplares SET estado_conservacao = 'BOM' WHERE estado_conservacao = 'Bom';
 UPDATE exemplares SET estado_conservacao = 'EXCELENTE' WHERE estado_conservacao = 'Excelente';
 UPDATE exemplares SET estado_conservacao = 'REGULAR' WHERE estado_conservacao = 'Regular';
 
--- Reabilitar safe mode
 SET SQL_SAFE_UPDATES = 1;
 
--- Verificar resultado
 SELECT DISTINCT estado_conservacao FROM exemplares;
-
--- Resultado esperado:
--- BOM
--- EXCELENTE
--- REGULAR
--- RUIM (se houver)
--- PESSIMO (se houver)
