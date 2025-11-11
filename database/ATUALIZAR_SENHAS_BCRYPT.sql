1e220d1def930dea2c500bb6c295929f5ba2f206
Kranus37 <samuel.resende6637@gmail.com>
2025-11-11 19:35:53 -0300

    Update ATUALIZAR_SENHAS_BCRYPT.sql

--- database/ATUALIZAR_SENHAS_BCRYPT.sql
+++ database/ATUALIZAR_SENHAS_BCRYPT.sql
@@ -1,12 +1,6 @@
--- ============================================================================
--- SCRIPT PARA ATUALIZAR SENHAS COM HASH BCRYPT
--- Biblioteca Imperial
+
 
 USE biblioteca_imperial;
-
--- Hash BCrypt para "senha123" (gerado com BCrypt strength 10)
--- Senha original: senha123
--- Hash: $2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy
 
 UPDATE usuarios 
 SET senha_hash = '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy'
@@ -17,7 +11,6 @@ WHERE email IN (
     'yurik.feitosa@biblioimp.org'
 );
 
--- Verifica as atualizações
 SELECT 
     id_usuario,
     nome_completo,
@@ -31,16 +24,3 @@ WHERE email IN (
     'raphael.simao@biblioimp.org',
     'yurik.feitosa@biblioimp.org'
 );
-
--- ============================================================================
--- INFORMAÇÕES IMPORTANTES
--- ============================================================================
--- 
--- Todos os 4 usuários agora têm a senha: senha123
--- 
--- Hash BCrypt usado: $2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy
--- 
--- Para gerar novos hashes BCrypt em Java:
--- BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
--- String hash = encoder.encode("sua_senha");
--- ============================================================================
